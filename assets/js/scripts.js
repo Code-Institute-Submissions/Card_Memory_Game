a773890b20a3a6155aa3957bc38440ffa021325f
RastaYoda <kandik29282726@gmail.com>
2020-05-22 21:26:04 +0000

    Fixed game breaking bugs and added more comments

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,3 @@
-
 /* Wait for the page to load before executing code */
 window.onload = function() {
 
@@ -20,23 +19,28 @@ window.onload = function() {
         /* Checks if the game is locked */
         if (lockGame === false) {
             flipsCount ++;
-            let id = this.id.split(" ");
+            let id = this.id.split(" ")[0];
             openCards.push(id);
-            
+
+            /* Remove interaction on clicked card */
+            $(this).addClass("inactive");
             /* Flip animation on clicked card*/
             $(this).css({"transform": "rotateY(360deg)"});
             $(this).find(".back").fadeOut(750);
             /* Adding a placeholder class to clicked card */
             $(this).addClass("open");
 
-            /* If two cards are flipped check pair, lock game and reset flipsCount */
+            /* If two cards flipped execute code */
             if (flipsCount === 2) {
+
+                /* Reset flipsCount and lock the game */
+                flipsCount = 0;
                 lockGame = true;
+                /* check if cards match */
+                checkPair();
+                /* Increase the turns counter and output it to the #turns div */
                 turnsCounter++;
                 $("#turns").html(turnsCounter);
-                console.log(turnsCounter);
-                checkPair();
-                flipsCount = 0;
             };
 
         } else {
@@ -47,10 +51,12 @@ window.onload = function() {
 
     /* Function to check if open cards match */
     function checkPair() {
-        if (openCards[0][0] === openCards[1][0]) {
+        if (openCards[0] === openCards[1]) {
             console.log("Match!");
-            openCards = [];
             $(".open").removeClass("open");
+            /* Remove game lock and reset openCards array */
+            lockGame = false;
+            openCards = [];
         } else {
             console.log("No match!");
             setTimeout(flipBack, 1500);
@@ -60,9 +66,13 @@ window.onload = function() {
     /* Function to flip back cards which don't match */
     function flipBack() {
         console.log("FlipBack function is being processed");
+        /* Flip back animation */
         $(".open").css({"transform": "rotateY(-360deg)"});
         $(".open").find(".back").fadeIn(750);
+        /* Remove placeholder class and add back interaction */
+        $(".open").removeClass("inactive");
         $(".open").removeClass("open");
+        /* Remove game lock and nulify openCards array */
         lockGame = false;
         openCards = [];
     };
