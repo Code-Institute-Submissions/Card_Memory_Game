f361229204eb2d555bca0c84540b32c426af365b
RastaYoda <kandik29282726@gmail.com>
2020-05-22 19:23:56 +0000

    Added game lock to avoid game breaking bugs

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,25 +6,33 @@ $("#welcome-btn").click(function() {
 window.onload = function() {
     let flipsCount = 0;
     let openCards = [];
-    
+    let lockGame = 0;
+
+    if (lockGame === 1) {}
+
     /* Code to be executed when a card is clicked on */
     $(".card").click(function() {
-        flipsCount ++;
-        let id = this.id.split(" ");
-        openCards.push(id);
-        
-        /* Flip animation on clicked card*/
-        $(this).css({"transform": "rotateY(-180deg)"});
-        $(this).find(".back").fadeOut(750);
-        /* Adding a placeholder class to clicked card */
-        $(this).addClass("open");
+        if (lockGame === 0) {
+            flipsCount ++;
+            let id = this.id.split(" ");
+            openCards.push(id);
+            
+            /* Flip animation on clicked card*/
+            $(this).css({"transform": "rotateY(360deg)"});
+            $(this).find(".back").fadeOut(750);
+            /* Adding a placeholder class to clicked card */
+            $(this).addClass("open");
 
-        /* If two cards are flipped check pair and reset flipsCount */
-        if (flipsCount === 2) {
-            checkPair()
-            flipsCount = 0
-        };
+            /* If two cards are flipped check pair and reset flipsCount */
+            if (flipsCount === 2) {
+                lockGame++;
+                checkPair();
+                flipsCount = 0;
+            };
 
+        } else {
+            
+        }
     });
 
 
@@ -36,16 +44,17 @@ window.onload = function() {
             $(".open").removeClass("open");
         } else {
             console.log("No match!");
-            flipBack();
+            setTimeout(flipBack, 2000);
         };
     };
 
     /* Function to flip back cards which don't match */
     function flipBack() {
-        console.log("Function is processed");
-            $(".open").delay(1000).css({"transform": "rotateY(180deg)"});
-            $(".open").find(".back").delay(1000).fadeIn(750);
-            $(".open").removeClass("open");
-            openCards = [];
+        console.log("FlipBack function is being processed");
+        $(".open").css({"transform": "rotateY(-360deg)"});
+        $(".open").find(".back").fadeIn(750);
+        $(".open").removeClass("open");
+        lockGame--;
+        openCards = [];
     };
 };
