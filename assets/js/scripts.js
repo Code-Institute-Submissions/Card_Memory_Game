c891be5b4730ef34fac267e23551dcea907777c0
RastaYoda <kandik29282726@gmail.com>
2020-05-22 17:18:25 +0000

    Added a function to check for matching pairs

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,11 +4,30 @@ $("#welcome-btn").click(function() {
 
 window.onload = function() {
     let flipsCount = 0;
+    let openCards = [];
 
+    
     $(".card").click(function() {
         flipsCount ++
-        let id = this.id
+        let id = this.id.split(" ")
+        openCards.push(id);
+        /* Flip animation on clicked card */
         $(this).css({'transform': 'rotateY(180deg)'})
         $(this).find(".back").fadeOut(750);
+
+        if (flipsCount === 2) {
+            checkPair()
+            flipsCount = 0
+        }
     });
+
+    function checkPair() {
+        console.log(openCards);
+        if (openCards[0][0] === openCards[1][0]) {
+            console.log("Match!")
+            openCards = [];
+        } else {
+            console.log("No match!")
+        }
+    };
 };
