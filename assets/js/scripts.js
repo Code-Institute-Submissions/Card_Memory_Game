3a5c119af76933a53fa3c219224253e7fb189408
RastaYoda <kandik29282726@gmail.com>
2020-05-22 13:11:12 +0000

    Finished animation on card click, removed unnecessary code

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,3 +2,13 @@ $("#welcome-btn").click(function() {
     $("#welcome-msg").fadeOut(1000);
 });
 
+window.onload = function() {
+    let flipsCount = 0;
+
+    $(".card").click(function() {
+        flipsCount ++
+        let id = this.id
+        $(this).css({'transform': 'rotateY(180deg)'})
+        $(this).find(".back").fadeOut(750);
+    });
+};
